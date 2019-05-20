4476023fd44cff66d9c09763a588c7bdfa8b738d
千葉 勇輝 <yuuki.chiba@fujifilm.com>
2019-05-20 18:14:54 +0900

    fix Orchestrator & Activity functions

--- PerformanceTestFunctions/TestOrchestrator.cs
+++ PerformanceTestFunctions/TestOrchestrator.cs
@@ -7,164 +7,87 @@ using Microsoft.Azure.WebJobs;
 using Microsoft.Azure.WebJobs.Extensions.Http;
 using Microsoft.Extensions.Logging;
 using Newtonsoft.Json;
+using ExecutionContext = Microsoft.Azure.WebJobs.ExecutionContext;
 
 namespace PerformanceTestFunctions
 {
     public static class TestOrchestrator
     {
-        public class QueueMessageModels
+        public class QueueMessageModel
         {
             [JsonProperty("key")]
             public long Key { get; set; }
         }
 
         [FunctionName("TestOrchestrator")]
-        public static async Task<string> RunOrchestrator(
-            [OrchestrationTrigger] DurableOrchestrationContext context, ILogger log)
+        public static async Task RunOrchestrator(
+            [OrchestrationTrigger] DurableOrchestrationContext context, ExecutionContext ctx, ILogger log)
         {
-            if (!context.IsReplaying) log.LogDebug($"*** Start Orch.");
+            var key = context.GetInput<QueueMessageModel>().Key;
+
+            log.LogDebug($"*** Start Orch. : [Key] {key}");
 
             var retryOptions = new RetryOptions(
                 firstRetryInterval: TimeSpan.FromSeconds(5),
-                maxNumberOfAttempts: 2);
-            retryOptions.BackoffCoefficient = 3;
-            retryOptions.RetryTimeout = TimeSpan.FromSeconds(20);
-            retryOptions.Handle = (ex) =>
+                maxNumberOfAttempts: 3)
             {
-                if (!context.IsReplaying) log.LogDebug($"*** Retry handling at Orch. : {ex.Message}");
-                return true;
+                BackoffCoefficient = 3
             };
 
-            // variables for debug
-            var throwEx = false;
-            var useDelay = false;
+            string str = await context.CallActivityWithRetryAsync<string>("Activity_Hello", retryOptions, key);
 
-            await context.CallActivityWithRetryAsync<string>("TestOrchestrator_Hello", retryOptions, ("World", throwEx, useDelay));
+            log.LogDebug($"*** Reach check point #1");
 
-            if (!context.IsReplaying) log.LogDebug($"*** Reach check point #1");
-
-            var outputs = new List<Task<string>>();
-
-            // with retry
-            outputs.Add(context.CallSubOrchestratorWithRetryAsync<string>("TestSubOrchestrator", retryOptions, "Tokyo"));
-            outputs.Add(context.CallSubOrchestratorWithRetryAsync<string>("TestSubOrchestrator", retryOptions, "Seattle"));
-            outputs.Add(context.CallSubOrchestratorWithRetryAsync<string>("TestSubOrchestrator", retryOptions, "London"));
-
-            try
+            var parallelTasks = new List<Task<object>>()
             {
-                var result = await Task.WhenAll(outputs);
-            }
-            catch (Exception ex)
-            {
-                if (!context.IsReplaying) log.LogDebug($"*** Catch exception: {ex.Message}");
-                // ignore exception
-            }
+                context.CallSubOrchestratorAsync<object>("TestSubOrchestrator", $"{str}A"),
+                context.CallSubOrchestratorAsync<object>("TestSubOrchestrator", $"{str}B"),
+                context.CallSubOrchestratorAsync<object>("TestSubOrchestrator", $"{str}C")
+            };
+            object[] result = await Task.WhenAll(parallelTasks);
 
-            if (!context.IsReplaying) log.LogDebug($"*** Reach check point #2");
+            log.LogDebug($"*** Reach check point #2");
 
-            await context.CallActivityAsync<string>("TestOrchestrator_Hello", ("World x2", false, false));
+            await context.CallActivityWithRetryAsync<string>("Activity_Hello", retryOptions, result[0]);
 
-            if (!context.IsReplaying) log.LogDebug($"*** End Orch.");
-
-            return "Succeeded";
+            log.LogDebug($"*** End Orch.");
         }
 
         [FunctionName("TestSubOrchestrator")]
         public static async Task<string> RunSubOrchestrator(
-            [OrchestrationTrigger] DurableOrchestrationContext context, ILogger log)
+            [OrchestrationTrigger] DurableOrchestrationContext context, ExecutionContext ctx, ILogger log)
         {
             var name = context.GetInput<string>();
 
-            if (!context.IsReplaying) log.LogDebug($"**** Start SubOrch. : {name}");
+            log.LogDebug($"**** Start SubOrch. : {name}");
 
             var retryOptions = new RetryOptions(
                 firstRetryInterval: TimeSpan.FromSeconds(5),
-                maxNumberOfAttempts: 2);
-            retryOptions.BackoffCoefficient = 3;
-            retryOptions.RetryTimeout = TimeSpan.FromSeconds(20);
-            retryOptions.Handle = (ex) =>
+                maxNumberOfAttempts: 3)
             {
-                if (!context.IsReplaying) log.LogDebug($"*** Retry handling at SubOrch. : {ex.Message}");
-                return true;
+                BackoffCoefficient = 3
             };
 
-            // variables for debug
-            var throwEx = false;
-            var useDelay = false;
+            var result = await context.CallActivityWithRetryAsync<string>("Activity_Hello", retryOptions, $"{name}#1");
+            result = await context.CallActivityWithRetryAsync<string>("Activity_Hello", retryOptions, $"{result}#2");
 
-            // Replace "hello" with the name of your Durable Activity Function.
-            // with retry
-            await context.CallActivityWithRetryAsync<string>("TestOrchestrator_Hello", retryOptions, (name, throwEx, useDelay));
+            log.LogDebug($"*** Reach check point at SubOrch. : {name}");
 
-            if (!context.IsReplaying) log.LogDebug($"*** Reach check point at SubOrch. : {name}");
-
-            // variables for debug
-            throwEx = false;
-            useDelay = false;
-
-            // timer setting for timeout
-            TimeSpan timeout = TimeSpan.FromSeconds(15);
-            DateTime deadline = context.CurrentUtcDateTime.Add(timeout);
-
-            using (var cts = new CancellationTokenSource())
-            {
-                // no retry
-                Task activityTask = context.CallActivityWithRetryAsync<string>("TestOrchestrator_Hello", retryOptions, ($"{name} x2", throwEx, useDelay));
-                Task timeoutTask = context.CreateTimer(deadline, cts.Token);
-
-                Task winner = await Task.WhenAny(activityTask, timeoutTask);
-                if (winner == activityTask)
-                {
-                    if (!context.IsReplaying) log.LogDebug($"**** Task Complete at SubOrch. : {name}");
-
-                    // success case
-                    cts.Cancel();
-                    return ((Task<string>)winner).Result;
-                }
-                else
-                {
-                    if (!context.IsReplaying) log.LogDebug($"**** Timeout at SubOrch. : {name}");
-
-                    // timeout case
-                    throw new Exception($"Timeout at {name}.");
-                }
-            }
+            return result;
         }
 
-        [FunctionName("TestOrchestrator_Hello")]
-        public static string SayHello([ActivityTrigger] DurableActivityContext context, ILogger log)
+        [FunctionName("Activity_Hello")]
+        public static string Reply([ActivityTrigger] DurableActivityContext context, ILogger log)
         {
-            var (name, throwEx, useDelay) = context.GetInput<(string, bool, bool)>();
+            var name = context.GetInput<string>();
 
             log.LogDebug($"***** Start Activity : {name}");
 
-            if (useDelay)
-            {
-                Thread.Sleep(TimeSpan.FromSeconds(30));
-                //Thread.Sleep(TimeSpan.FromSeconds(330)); // over 5 minutes
-            }
-
-            if (throwEx)
-            {
-                log.LogDebug($"***** Exception at Activity : {name}");
-
-                try
-                {
-                    throw new Exception($"***** Exception occurred at {name}.");
-                }
-                catch (Exception e)
-                {
-                    log.LogError(e, $"{e.Message}\r\n{e.StackTrace}");
-                    throw;
-                }
-            }
-
-            log.LogInformation($"***** Saying hello to {name}.");
-            return $"Hello {name}!";
+            return $"{name}";
         }
 
         [FunctionName("QueueStart")]
-        public static async Task Run([QueueTrigger("durable-function-trigger")]QueueMessageModels myQueueItem,
+        public static async Task Run([QueueTrigger("durable-function-trigger")]QueueMessageModel myQueueItem,
             [OrchestrationClient]DurableOrchestrationClient starter, ILogger log)
         {
             log.LogInformation($"C# Queue trigger function processed:  [Key] {myQueueItem.Key}");
